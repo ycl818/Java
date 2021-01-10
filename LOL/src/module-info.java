ffc8ff9fb6363d96b055876aa5f18a3dec7a132e
yichien <kevinyichienlee@gmail.com>
2021-01-10 09:47:43 +0800

    ++

---
+++ LOL/src/module-info.java
@@ -0,0 +1,2 @@
+module lol {
+}
---
+++ java337/src/HelloWorldDemo.java
@@ -0,0 +1,12 @@
+
+public class HelloWorldDemo {
+
+    public static void	main(String[] args){
+    	double x = 1.0/2;
+    	System.out.println(x);
+	}
+	
+	
+	
+	
+}
